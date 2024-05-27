13e7fb8539e0f7087fd34cd1cee86c17c707affa
DenSan83 <dennis.santillan83@gmail.com>
2024-05-27 11:11:08 -0400

    ADD airline, airport and flight models

---
+++ database/migrations/2024_05_27_150517_create_flights_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->string('airline', 2);
+            $table->string('number', 4);
+            $table->string('departure_airport');
+            $table->time('departure_time');
+            $table->string('arrival_airport');
+            $table->time('arrival_time');
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flights');
+    }
+};
